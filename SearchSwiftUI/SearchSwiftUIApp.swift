b593dee248dd8e1edd13de52100cdd4c20bc1243
Apurva Jalgaonkar <apurva.jalgaonkar@sportzinteractive.net>
2024-07-11 12:55:52 +0530

    Initial Commit

---
+++ SearchSwiftUI/SearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIApp.swift
+//  SearchSwiftUI
+//
+//  Created by Apurva Jalgaonkar on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
